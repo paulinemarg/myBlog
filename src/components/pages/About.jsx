9862f8ffd9df1aa30bc8dd36d3c02f8ff43d0fd9
paulinemarg <paulinemargossian@gmail.com>
2022-08-05 18:24:08 +0200

    added about me section

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
-import '../../App.css';
+import './About.css';
 
 export default function About() {
-  return <h1 className='about'>About Me</h1>;
+  return (
+    <div className='about'>
+      <div className="polaroid">
+        <h1>About Me</h1>
+        <img src='images/pauline.jpg' alt="pauline profile pic"/>
+        <div className="container">
+          <p>Hi there! Welcome to my Blog!<br></br>
+          I am Pauline! A developer and designer.<br></br>
+          I have never thought of writing a blog until recently, when my mentor gave me feedback on my case studies and pointed out that I have exceptional skills of storytelling and presentations. So I have decided to put my skill in use!    
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
