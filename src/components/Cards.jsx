47076ac05bf353fdc0222d6674624c74c254cbd1
paulinemarg <paulinemargossian@gmail.com>
2022-08-12 11:44:42 +0200

    added path to article

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -13,13 +13,13 @@ function Cards() {
               src='images/books.jpg' // Photo from Unsplash
               text='Books that helped me during UI/UX'
               label='New'
-              path='/articles'
+              path='/'
             />
             <CardItem
               src='images/motivation.jpg' // Photo by Cristofer Maximilian on Unsplash
               text='How I stay motivated'
               label='31 July 2022'
-              path='/articles'
+              path='/'
             />
           </ul>
           <ul className='cards__items'>
@@ -27,19 +27,19 @@ function Cards() {
               src='images/growth.jpg' // Photo by Kyle Glenn on Unsplash
               text='Planning my professional development'
               label='24 July 2022'
-              path='/articles'
+              path='/'
             />
             <CardItem
               src='images/possible.jpg' // Photo by Towfiqu barbhuiya on Unsplash
               text='How challenging is it to come from a Non-Tech Background'
               label='17 July 2022'
-              path='/articles'
+              path='/'
             />
             <CardItem
               src='images/coding.jpg' // Photo by AltumCode on Unsplash
-              text='How I entered the Tech World'
+              text='How I Entered the Tech World'
               label='10 July 2022'
-              path='/articles'
+              path='/tech'
             />
           </ul>
         </div>
