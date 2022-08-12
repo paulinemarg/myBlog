6062a1e9762bd7cd2f9f22570be5827d8c4c926a
paulinemarg <paulinemargossian@gmail.com>
2022-08-12 11:44:55 +0200

    added a new article

---
+++ src/components/pages/Tech.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Tech.css';
+
+export default function Tech() {
+  return (
+    <div className='tech'>
+      <h1>How I Entered the Tech World</h1>
+        <img src='images/laptop.jpg' alt='' />
+        <div className="tech-content">
+          <div className='indent'>
+            I am a business graduate with a 6 years of experience as an insurance consultant. I loved the insurance industry, because I love solving problems and finding solutions, in this case determining the insurance coverage that best met client's need.
+          </div>
+          <div className='indent'>
+            Last year I have decided to make a career change, I still wanted to solve problems but in a creative way. My friend sent me a link to CareerFoundry's <a href='https://careerfoundry.com/en/courses/become-a-web-developer/'>Full-Stack Development Program</a> and told me I should apply "you are the smartest one I know and I know you can do it". These words hit differently even though I had no previous programming knowledge or anything related, but I was curious to find out. So I have enrolled in the program, I had to submit 5 tasks in 10 days (Trial Period), I have submitted 5 tasks in 5 days! I was amazed how natural felt coding, the more I faced challenges and obstacles the more I was hooked and needed to find solutions.
+          </div>
+          <div className='indent'>
+            Learning to code gave me a newfound appreciation of the work that gets put into technology that many take for granted (including me before entering the tech world), I love the idea of bringing my own imagination to life and developing my own technical solutions. The opportunity to create something that can be seen and used by millions and somehow makes a difference in their lives makes me happy.
+          </div>
+          <div className='indent'>
+            I have a lot to learn and keep up with the fast paced tech industry but I am ready for it! I am eager and willing to learn and enhance my new gained skills and also to master new ones along the way.
+          </div>
+        </div>
+    </div>
+  );
+}
