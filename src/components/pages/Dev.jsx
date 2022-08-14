bd5aa25db6cda80041061d71e5fba5ab1644a10e
paulinemarg <paulinemargossian@gmail.com>
2022-08-14 12:01:43 +0200

    Create Dev.jsx

---
+++ src/components/pages/Dev.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Dev() {
+  return (
+    <div className='article'>
+      <h1>Planning My Professional Development</h1>
+        <img src='images/growth.jpg' alt='' />
+        <div className="article-content">
+          <div className='indent'>
+            As I started learning to code I became so excited that I wanted to learn everything all at once, like new programming languages, libraries and frameworks. But then I realised by focusing on learning new things I was neglecting my newly gained skills which still needed development and enhancement. 
+            Therefore I have decided to work on my newly gained skills first and when I feel that I have mastered them I can move on to new ones.
+          </div>
+          <div className='indent'>
+            <h3>Ways I Improve My Skils:</h3>
+            <li>Online Courses (There are a lot of free as well as paid courses available online)</li>
+            <li>Watching youtube videos</li>
+            <li>Reading articles and documentations</li>
+            <li>Testing myself with quizes and coding challenges</li>
+            <li>Volunteering as a web developer for an NGO, for a friend who needs a website for their startup, etc... (Even though you're not getting paid but you are getting a chance to practice your skills in a real world projects!)</li>
+          </div>
+          <div className='indent'>
+            <h3>Maintaining Portfolio</h3>
+            Besides enhancing and developing skills, it's always important to update your portfolio, like updating existing projects, adding new ones, writing case studies and even starting a blog if you like writing. But it's important to maintain your portfolio because it is one of the most vital things you have to 
+            show off your skills and impress potential employers.
+          </div>
+        </div>
+    </div>
+  );
+}
