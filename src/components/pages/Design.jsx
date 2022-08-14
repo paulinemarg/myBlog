4d811dc4034c5332fa16c6ced1e502c030165087
paulinemarg <paulinemargossian@gmail.com>
2022-08-14 12:01:40 +0200

    Create Design.jsx

---
+++ src/components/pages/Design.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Design() {
+  return (
+    <div className='article'>
+      <h1>Why UI Design?</h1>
+        <img src='images/design.jpg' alt='' />
+        <div className="article-content">
+          <div className='indent'>
+          </div>
+        </div>
+    </div>
+  );
+}
