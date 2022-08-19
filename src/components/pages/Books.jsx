2354a512f42178d72929cea0a8ebd600421391d7
paulinemarg <paulinemargossian@gmail.com>
2022-08-19 16:12:33 +0200

    created new page

---
+++ src/components/pages/Books.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Books() {
+  return (
+    <div className='article'>
+      <h1>Books that helped me during UI/UX</h1>
+        <img src='images/books.jpg' alt='' />
+        <div className="article-content">
+          <div className='indent'>
+          </div>
+        </div>
+    </div>
+  );
+}
