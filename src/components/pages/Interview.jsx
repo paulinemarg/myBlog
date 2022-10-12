40ce90e21bde18714b5ed759517b5e55a30a0a35
paulinemarg <paulinemargossian@gmail.com>
2022-10-12 20:30:14 +0200

    Corrected typo

--- src/components/pages/Interview.jsx
+++ src/components/pages/Interview.jsx
@@ -9,7 +9,7 @@ export default function Interview() {
         <div className="article-content">
           <div className='indent'>
             Interviews can be really stressful, cause anxiety and drain your energy. There are many factors that cause these emotions like meeting strangers in a position of authority, the fear that you are going to be visibly nervous and probably embarrass yourself, talking about yourself and your abilities and so on. 
-            These emotions will not disappear on their own and it will take a lot of time and practice to overcome them. However, there are some things you can do to ease the process, but you have to find your own way and what makes you comfortable. Things that make me relax, doesn't have to do the same for you because we are all different, but they can insipre you to find yours.
+            These emotions will not disappear on their own and it will take a lot of time and practice to overcome them. However, there are some things you can do to ease the process, but you have to find your own way and what makes you comfortable. Things that make me relax, doesn't have to do the same for you because we are all different, but they can inspire you to find yours.
           </div>
           <div>
             <li>Give your body what it needs to feel its best, which is rest and fuel </li>
